aa92b72dbfafe56663935d1643d5647f8e19c723
Kuslesath <92498442+Kuslesath@users.noreply.github.com>
2021-11-21 20:38:04 +0530

    Add files via upload

---
+++ Spring Assignments/Day2_Assignments_Spring_MVC/HelloController.java
@@ -0,0 +1,50 @@
+package com.sonata.springMvc;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.sql.*;
+import com.sonata.casestudy.impl.userDaoImpl;
+
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/index")
+	public ModelAndView index() {
+			
+			ModelAndView mav = new ModelAndView("index");
+			return mav;
+	}
+	
+	@RequestMapping("/login")
+	public ModelAndView login(@RequestParam("username") String username,@RequestParam("password") String password, Model model) {
+		ModelAndView mav = new ModelAndView("Welcome");
+		model.addAttribute("username", username);
+		model.addAttribute("password", password);
+		
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc_casestudy","root","admin@123");
+			PreparedStatement p1=con.prepareStatement("select userid from user where username=? and pwd=?");
+			p1.setString(1, username);
+			p1.setString(2, password);
+			ResultSet rs=p1.executeQuery();
+			if(rs.next()) {
+				
+				return mav;
+			}
+		}
+		catch(Exception e) {
+			System.out.print(e);
+		}
+		return new ModelAndView("NewFile");
+	}
+}
